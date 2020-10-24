35918a023f76d13bcdc283bc712ff01857bc2a72
Dejan Levec <dejan@celtra.com>
2020-10-24 23:25:05 +0200

    provision nlb instead of elb, fix helm source

--- traefik.tf
+++ traefik.tf
@@ -32,6 +32,16 @@ resource "helm_release" "traefik" {
             protocol    = "TCP"
           }
         }
+
+        service = {
+          enabled = "true"
+          type    = "LoadBalancer"
+          annotations = {
+            "service.beta.kubernetes.io/aws-load-balancer-backend-protocol" = "tcp",
+            "service.beta.kubernetes.io/aws-load-balancer-type"             = "nlb",
+            "service.beta.kubernetes.io/aws-load-balancer-proxy-protocol"   = "*",
+          }
+        }
       }
     )
   ]
